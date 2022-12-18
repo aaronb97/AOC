5ac1c8af77ac2cc9487e94cfe5563c96d583f2fe
Aaron Becker <23224355+aaronb97@users.noreply.github.com>
2022-12-18 12:11:36 -0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { logResult } from "./logResult";
+import { ints } from "./helpers";
+
+function main(input: string, ...args: unknown[]) {
+  console.log(args[0]);
+  let sum = 0;
+  const split = input.split("\n");
+
+  for (const line of split) {
+    sum++;
+  }
+
+  return sum;
+}
+
+logResult("Main", "inputs/input.txt", main, 1);
+logResult("Test", "inputs/testInput.txt", main);
