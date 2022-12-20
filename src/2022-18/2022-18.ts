5ac8ece94253cb4b71102540af7af08b2c8160bf
aaronb97 <aaron9becker7@gmail.com>
2022-12-20 10:30:49 -0500

    day 18 part 2

--- src/2022-18/2022-18.ts
+++ src/2022-18/2022-18.ts
@@ -33,9 +33,53 @@ function part1(input: string) {
   return sum;
 }
 
+function part2(input: string) {
+  const split = input.split("\n");
+  const set = new Set<string>();
+  const visitedSet = new Set<string>();
+
+  const cubes = split.map((x) => ints(x));
+  cubes.forEach((cube) => set.add(`${cube[0]},${cube[1]},${cube[2]}`));
+
+  let sum = 0;
+  const sides = [
+    [-1, 0, 0],
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, -1, 0],
+    [0, 0, 1],
+    [0, 0, -1],
+  ];
+
+  const max = Math.max(...ints(input)) + 1;
+
+  const queue = [[max, max, max]];
+
+  while (queue.length) {
+    const q = queue.shift() as [number, number, number];
+    for (const side of sides) {
+      const check = [side[0] + q[0], side[1] + q[1], side[2] + q[2]];
+      if (check.some((checkSide) => checkSide > max || checkSide < -1)) {
+        continue;
+      }
+
+      const checkString = `${check[0]},${check[1]},${check[2]}`;
+
+      if (set.has(checkString)) {
+        sum++;
+      } else if (!visitedSet.has(checkString)) {
+        queue.push(check);
+        visitedSet.add(checkString);
+      }
+    }
+  }
+
+  return sum;
+}
+
 export default function main() {
-  logResult("Test", __dirname + "/testInput.txt", part1);
-  logResult("Main", __dirname + "/input.txt", part1);
+  logResult("Test", __dirname + "/testInput.txt", part2);
+  logResult("Main", __dirname + "/input.txt", part2);
   // logResult("Test", __dirname + "/input.txt", part1);
   // logResult("Test", __dirname + "/testInput.txt", part2);
   //   logResult("Main", __dirname + "/input.txt", part2);
