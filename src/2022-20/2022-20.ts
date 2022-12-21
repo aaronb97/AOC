720e74b2fa59471af794c1a108066971b37ad146
aaronb97 <aaron9becker7@gmail.com>
2022-12-21 13:48:40 -0500

    day 20 part 2

--- src/2022-20/2022-20.ts
+++ src/2022-20/2022-20.ts
@@ -27,11 +27,39 @@ function part1(input: string) {
   );
 }
 
-function part2(input: string) {}
+function part2(input: string) {
+  const nums = ints(input);
+
+  const numObjs = nums.map((num) => ({ value: num * 811589153 }));
+  const queue = [];
+
+  for (let i = 0; i < 10; i++) {
+    queue.push(...numObjs.map((obj) => obj));
+  }
+
+  while (queue.length) {
+    const e = queue.shift();
+    if (!e) throw new Error("something happened");
+
+    const index = numObjs.indexOf(e);
+    const newIndex = (index + e.value) % (numObjs.length - 1);
+
+    numObjs.splice(index, 1);
+    numObjs.splice(newIndex, 0, e);
+  }
+
+  const start = numObjs.findIndex((x) => x.value === 0);
+
+  return (
+    numObjs[(start + 1000) % numObjs.length].value +
+    numObjs[(start + 2000) % numObjs.length].value +
+    numObjs[(start + 3000) % numObjs.length].value
+  );
+}
 
 export default function main() {
-  logResult("Test", __dirname + "/testInput.txt", part1);
-  logResult("Main", __dirname + "/input.txt", part1);
-  //   logResult("Main", __dirname + "/testInput.txt", part2);
-  //   logResult("Test", __dirname + "/input.txt", part2);
+  // logResult("Test", __dirname + "/testInput.txt", part1);
+  // logResult("Main", __dirname + "/input.txt", part1);
+  logResult("Main", __dirname + "/testInput.txt", part2);
+  logResult("Test", __dirname + "/input.txt", part2);
 }
