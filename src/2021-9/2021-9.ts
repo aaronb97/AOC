b33f3261e2accb9c27071471c90413a405686e2d
aaronb97 <aaron9becker7@gmail.com>
2023-01-14 18:38:38 -0500

    2021-9 part 2

--- src/2021-9/2021-9.ts
+++ src/2021-9/2021-9.ts
@@ -22,11 +22,59 @@ function part1(input: string) {
   return sum;
 }
 
-function part2(input: string) {}
+function part2(input: string) {
+  const grid = stringTo2DArray(input, (c) => Number(c));
+
+  const lows = [];
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      const num = grid[i][j];
+
+      const isLow = DIRS.every(
+        (dir) => (grid[i + dir[0]]?.[j + dir[1]] ?? 10) > num
+      );
+      if (isLow) {
+        lows.push([i, j]);
+      }
+    }
+  }
+
+  const sizes = lows.map((low) => findBasinSize(grid, low));
+  sizes.sort((a, b) => b - a);
+  return sizes[0] * sizes[1] * sizes[2];
+}
+
+function findBasinSize(grid: number[][], low: number[]) {
+  const queue = [low];
+  const visited = new Set();
+
+  let size = 0;
+  while (queue.length) {
+    size++;
+    const cell = queue.pop() as number[];
+
+    for (const dir of DIRS) {
+      const X = cell[0] + dir[0],
+        Y = cell[1] + dir[1];
+      const num = grid[X]?.[Y] ?? 10;
+      if (
+        num > grid[cell[0]][cell[1]] &&
+        num < 9 &&
+        !visited.has(`${X},${Y}`)
+      ) {
+        queue.push([X, Y]);
+        visited.add(`${X},${Y}`);
+      }
+    }
+  }
+
+  return size;
+}
 
 export default function main() {
   logResult("Test", __dirname + "/testInput.txt", part1);
   logResult("Main", __dirname + "/input.txt", part1);
-  //   logResult("Main", __dirname + "/testInput.txt", part2);
-  //   logResult("Test", __dirname + "/input.txt", part2);
+  logResult("Test", __dirname + "/testInput.txt", part2);
+  logResult("Main", __dirname + "/input.txt", part2);
 }
