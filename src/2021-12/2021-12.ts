45e9c452a425ce26489021ed7565e6caf12fcc56
aaronb97 <aaron9becker7@gmail.com>
2023-01-16 10:13:18 -0500

    2021 day 12 part 2

--- src/2021-12/2021-12.ts
+++ src/2021-12/2021-12.ts
@@ -43,11 +43,64 @@ function part1(input: string) {
   return count;
 }
 
-function part2(input: string) {}
+function part2(input: string) {
+  const split = input.split("\n");
+
+  const map: Record<string, string[]> = {};
+
+  for (const line of split) {
+    const [to, from] = line.split("-");
+    if (!map[to]) map[to] = [];
+    if (!map[from]) map[from] = [];
+
+    map[to].push(from);
+    map[from].push(to);
+  }
+
+  const visitedCount: Record<string, number> = {};
+  const paths = new Set<string>();
+
+  function traverse(node: string, twiceNode: string, path: string[] = []) {
+    if (node === "end") {
+      paths.add(path.join());
+      return;
+    }
+
+    if (node === twiceNode) {
+      if (visitedCount[node] === 2) {
+        return;
+      }
+    } else if (visitedCount[node]) {
+      return;
+    }
+
+    if (isLowerCase(node)) {
+      if (visitedCount[node] === undefined) {
+        visitedCount[node] = 1;
+      } else {
+        visitedCount[node]++;
+      }
+    }
+
+    for (const newNode of map[node]) {
+      traverse(newNode, twiceNode, [...path, newNode]);
+    }
+
+    visitedCount[node]--;
+  }
+
+  for (const key of Object.keys(map)
+    .filter(isLowerCase)
+    .filter((x) => x !== "start" && x !== "end")) {
+    traverse("start", key);
+  }
+
+  return paths.size;
+}
 
 export default function main() {
-  logResult("Test part 1", __dirname + "/testInput.txt", part1);
-  logResult("Main part 1", __dirname + "/input.txt", part1);
-  //   logResult("Test part 2", __dirname + "/testInput.txt", part2);
-  //   logResult("Main part 2", __dirname + "/input.txt", part2);
+  // logResult("Test part 1", __dirname + "/testInput.txt", part1);
+  // logResult("Main part 1", __dirname + "/input.txt", part1);
+  logResult("Test part 2", __dirname + "/testInput.txt", part2);
+  logResult("Main part 2", __dirname + "/input.txt", part2);
 }
