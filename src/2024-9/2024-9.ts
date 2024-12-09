b62d6436e3a038f13112f4745a83698cee71a7d2
Aaron Becker <aaron9becker7@gmail.com>
2024-12-09 16:36:51 -0500

    day 9 part 2

--- src/2024-9/2024-9.ts
+++ src/2024-9/2024-9.ts
@@ -52,6 +52,66 @@ export function part1(input: string, _visualize?: (toVis: unknown) => void) {
 }
 
 export function part2(input: string, _visualize?: (toVis: unknown) => void) {
-  const split = input.split("\n");
-  return ints(input);
+  const map: { digit: number; index: number; length: number }[] = [];
+
+  const nums = input.split("").map(Number);
+
+  let indexCounter = 0;
+
+  let max = 0;
+
+  for (let i = 0; i < nums.length; i += 2) {
+    const digit = i / 2;
+    max = digit;
+    const length = nums[i];
+    const spaces = nums[i + 1];
+
+    map.push({ length, index: indexCounter, digit });
+
+    indexCounter += length + spaces;
+  }
+
+  for (let i = max; i >= 0; i--) {
+    let digit: (typeof map)[number] | undefined = undefined;
+    let digitIndex = -1;
+
+    digit = undefined;
+    digitIndex = -1;
+
+    for (let j = 0; j < map.length; j++) {
+      if (map[j].digit === i) {
+        digit = map[j];
+        digitIndex = j;
+        break;
+      }
+    }
+
+    if (!digit) {
+      break;
+    }
+
+    // _visualize?.({ map });
+
+    for (let j = 0; j < map.length - 1; j++) {
+      if (digit === map[j]) break;
+
+      if (map[j + 1].index - (map[j].index + map[j].length) >= digit.length) {
+        digit.index = map[j].index + map[j].length;
+        map.splice(digitIndex, 1);
+        map.splice(j + 1, 0, digit);
+        break;
+      }
+    }
+  }
+
+  let sum = 0;
+
+  for (let i = 0; i < map.length; i++) {
+    const entry = map[i];
+    for (let j = entry.index; j < entry.index + entry.length; j++) {
+      sum += j * entry.digit;
+    }
+  }
+
+  return sum;
 }
